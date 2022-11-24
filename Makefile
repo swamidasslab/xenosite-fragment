d33d183e76b73db68a1694b69d51f0079740c916
S. Joshua Swamidass <swamidass@gmail.com>
2022-11-24 04:02:09 +0000

    cli updates and upgrades

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	python -m xenosite.fragment.cli --ring --max-size=7 bioactivation_dataset.csv ring_network.pkl.gz
--- xenosite/fragment/cli.py
+++ xenosite/fragment/cli.py
@@ -1,5 +1,5 @@
 from xenosite.fragment import FragmentNetworkX, RingFragmentNetworkX
-from tqdm import tqdm # type: ignore
+from tqdm import tqdm  # type: ignore
 import ray
 import typer
 import ast
@@ -12,99 +12,110 @@ DATA = os.environ.get("DATA", None)
 
 app = typer.Typer()
 
+
 @ray.remote(max_calls=200)
-def one_frag_network(smiles, rids, max_size : int, ring : bool):
+def one_frag_network(smiles, rids, max_size: int, ring: bool):
 
-  NetworkClass = RingFragmentNetworkX if ring else FragmentNetworkX
+    NetworkClass = RingFragmentNetworkX if ring else FragmentNetworkX
 
-  try:
-    return NetworkClass(smiles, marked=rids, max_size=max_size)
-  except Exception:
-    print("Failed on:", smiles, rids)
-    return NetworkClass(max_size=max_size)
-  
+    try:
+        return NetworkClass(smiles, marked=rids, max_size=max_size)
+    except Exception:
+        print("Failed on:", smiles, rids)
+        return NetworkClass(max_size=max_size)
+
 
 def ray_apply(seq, launch_job, concurrent):
-  result_refs = []
-  ready = []
+    result_refs = []
+    ready = []
 
-  with tqdm(total=len(seq), ncols=65) as pbar:
+    with tqdm(total=len(seq), ncols=65) as pbar:
 
-    for s in seq:
-      if len(result_refs) >= concurrent:
-        ready, result_refs = ray.wait(result_refs, fetch_local=True)
+        for s in seq:
+            if len(result_refs) >= concurrent:
+                ready, result_refs = ray.wait(result_refs, fetch_local=True)
 
-      result_refs.append(launch_job(s))
+            result_refs.append(launch_job(s))
 
-      while ready:
-        pbar.update(1)
-        yield ready.pop()
+            while ready:
+                pbar.update(1)
+                yield ready.pop()
 
-    while result_refs:
-      ready, result_refs = ray.wait(result_refs, fetch_local=True)
+        while result_refs:
+            ready, result_refs = ray.wait(result_refs, fetch_local=True)
 
-      while ready:
-        pbar.update(1)
-        yield ready.pop()
+            while ready:
+                pbar.update(1)
+                yield ready.pop()
 
-    assert len(result_refs) == 0
-
+        assert len(result_refs) == 0
 
 
 def read_data(filename: str):
-  data = []
-  with open(filename) as f:
-    reader = csv.reader(f, dialect="excel-tab")
+    data = []
+    with open(filename) as f:
+        reader = csv.reader(f, dialect="excel-tab")
 
-    for R in reader:
-      smi, sor = R
-      sor = ast.literal_eval(sor)
-      sor = {s[0] for s in sor}
+        for R in reader:
+            smi, sor = R
+            sor = ast.literal_eval(sor)
+            sor = {s[0] for s in sor}
 
-      smi = smi.split(".")[0]
-      data.append((smi, sor))
+            smi = smi.split(".")[0]
+            data.append((smi, sor))
 
-  return data
+    return data
 
 
 @app.command()
 def main(
-  input: str = typer.Argument("bioactivation_dataset.csv"), 
-  output: str = typer.Argument("network.pkl.gz"), 
-  max_size : int = 12, 
-  directory : Optional[str] = DATA,
-  concurrent : int = 30,
-  ring : bool = False):
+    input: str = typer.Argument("bioactivation_dataset.csv"),
+    output: str = typer.Argument("network.pkl.gz"),
+    max_size: int = 12,
+    directory: Optional[str] = DATA,
+    concurrent: int = 30,
+    ring: bool = False,
+    filter_unmarked: bool = True,
+):
 
-  NetworkClass = RingFragmentNetworkX if ring else FragmentNetworkX
+    NetworkClass = RingFragmentNetworkX if ring else FragmentNetworkX
 
-  ray.init(runtime_env=runtime_env)
+    ray.init(runtime_env=runtime_env)
 
-  if directory:
-    input = os.path.join(directory, input)
-    output = os.path.join(directory, output)
+    if directory:
+        input = os.path.join(directory, input)
+        output = os.path.join(directory, output)
 
-  data = read_data(input)
-  result = NetworkClass(max_size=max_size)
+    data = read_data(input)
+    result = NetworkClass(max_size=max_size)
 
-  for frag_graph in ray_apply(
-    data,
-    lambda d: one_frag_network.remote(d[0], d[1], max_size, ring), 
-    concurrent=concurrent
-  ):
-    R = ray.get(frag_graph)
-    result.update(R)
-    del R
-  
-  result.save(output)
+    for frag_graph in ray_apply(
+        data,
+        lambda d: one_frag_network.remote(d[0], d[1], max_size, ring),
+        concurrent=concurrent,
+    ):
+        R = ray.get(frag_graph)
+        result.update(R)
+        del R
+
+    if filter_unmarked:
+        unmarked = [
+            frag
+            for frag in result.network
+            if not result.network.nodes[frag]["marked_count"]
+        ]
+        result.network.remove_nodes_from(unmarked)
+
+    result.save(output)
+
 
 runtime_env = {
-  "pip": ["numba", "rdkit", "networkx"],
-  "working_dir": "./",
-  "exclude": ["*.csv", ".git", "*.ipynb", "*.gz"],
-  "eager_install": True
+    "pip": ["numba", "rdkit", "networkx"],
+    "working_dir": "./",
+    "exclude": ["*.csv", ".git", "*.ipynb", "*.gz"],
+    "eager_install": True,
 }
 
 
 if __name__ == "__main__":
-  app()
+    app()
