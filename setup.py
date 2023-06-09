7911f62904abd2649cadeda37b2655ec890ab36d
S. Joshua Swamidass <swamidass@gmail.com>
2023-06-09 13:48:51 -0500

    added pandas requirement

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         ],
         "xenosite_command": ["fragment=xenosite.fragment.__main__:app"],
     },
-    install_requires=["rdkit", "numpy", "numba", "rich", "typer", "networkx"],
+    install_requires=["rdkit", "numpy", "numba", "rich", "typer", "networkx", "pandas"],
     classifiers=[
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
