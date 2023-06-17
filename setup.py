5b253a8cf7f72ee7d8957d0bed33384c41081d1e
S. Joshua Swamidass <swamidass@gmail.com>
2023-06-17 17:01:04 -0500

    added pynauty dependency

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         ],
         "xenosite_command": ["fragment=xenosite.fragment.__main__:app"],
     },
-    install_requires=["rdkit", "numpy", "numba", "rich", "typer", "networkx", "pandas"],
+    install_requires=["rdkit", "numpy", "numba", "rich", "typer", "networkx", "pandas", "pynauty==2.8.6"],
     classifiers=[
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
